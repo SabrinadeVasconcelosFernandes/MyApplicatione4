26a7006e1196e70558ebad70de6f80208369cd52
sabrinadevasconcelosfernandes <sabrinadvf@ibm.com>
2022-03-07 15:48:08 -0300

    Initial commit

---
+++ app/src/main/java/com/sabsrocambole/myapplicationo/Controller/CreateUserActivity.kt
@@ -0,0 +1,57 @@
+package com.sabsrocambole.myapplicationo.Controller
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import com.sabsrocambole.myapplicatione4.R
+import java.util.*
+
+
+class CreateUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    var createAvatarImageView = findViewById<ImageView>(R.id.createAvatarImageView)
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+
+    fun generateUserAvatar(view: View){
+        val random = Random()
+        val color = random.nextInt(2)
+        val avatar = random.nextInt(28)
+
+        if (color == 0){
+            userAvatar = "Light$avatar"
+        }
+        else{
+            userAvatar = "dark$avatar"
+        }
+        val resourceId = resources.getIdentifier(userAvatar,"drawable",packageName)
+        createAvatarImageView.setImageResource(resourceId)
+    }
+
+    fun createUserClicked(view: View){
+        val random = Random()
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
+
+        createAvatarImageView.setBackgroundColor(Color.rgb(r,g,b))
+
+        val savedR = r.toDouble()/255
+        val savedG = g.toDouble()/255
+        val savedB = b.toDouble()/255
+
+        avatarColor = "[$savedR,$savedG,$savedB,1]"
+
+    }
+
+    fun generateColorClicked(view: View){
+
+    }
+
+}
---
+++ app/src/main/java/com/sabsrocambole/myapplicationo/Controller/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.sabsrocambole.myapplicationo.Controller
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.sabsrocambole.myapplicatione4.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginCreateUserBtnClicked(view: View){
+
+    }
+    fun loginLoginBtnClicked(view: View){
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
+    }
+}
